d8766ac7f0361bd677eb474610224a6543795cdf
scopevale <scopevale@gmail.com>
2013-04-24 12:33:41 +0100

    edited sam_jq_gantt.js

--- js/sam_jq_gantt.js
+++ js/sam_jq_gantt.js
@@ -1,20 +1,31 @@
 ;(function($, window, undefined) {
   var plugin = $.fn.gantt.prototype;
 	
+
+    
+
 	// store copies of the original plugin functions before overwriting
-	var functions = {};
-	for (var i in plugin) {
-		if (typeof(plugin[i]) === 'function') {
-			functions[i] = plugin[i];
-		}
-	}
+//	var functions = {};
+//	for (var i in plugin) {
+//		if (typeof(plugin[i]) === 'function') {
+//			functions[i] = plugin[i];
+//		}
+//	}
     
 //    var core = plugin.core;
 	
 	// extend existing functionality of the jquery-gantt plugin
 	$.extend(true, plugin, {
         
-        var core = 
+            var core = {
+
+		create: function(element) {
+		    plugin.core.create.apply(this, arguments);
+		    
+		    alert('were in the extension now!');
+		}
+	    };
+        });
         
         
         
@@ -23,70 +34,7 @@
         
         
         
-//		_create: function () {
-//			var self = this,
-//				doc = this.element[0].ownerDocument;
-//			
-//			functions._create.apply(this, arguments);
-//			
-//			this.hintText = this.element.data('hint');
-//			this.searchText = this.element.data('searching');
-//			
-//			// set up the hint object
-//			this.hint = $('<div></div>')
-//				.addClass('ui-autocomplete-hint')
-//				.appendTo($(this.options.appendTo || 'body', doc)[0]);
-//			
-//			// show/hide hint text on focus/blur
-//			this.element
-//				.bind('focus.autocomplete', function () {
-//					self._showHint(self.hintText);
-//				})
-//				.bind('blur.autocomplete', function () {
-//					self._hideHint();
-//				});
-//		},
-//		
-//		_destroy: function () {
-//			this.hint.remove();
-//			return functions._destroy.apply(this, arguments);
-//		},
-//		
-//		_search: function () {
-//			this._showHint(this.searchText);
-//			return functions._search.apply(this, arguments);
-//		},
-//		
-//		_response: function () {
-//			this._hideHint();
-//			return functions._response.apply(this, arguments);
-//		},
-//		
-//		_showHint: function (text) {
-//			text = $.trim(text);
-//			
-//			if (text !== '') {
-//				this.hint.text(text).show();
-//				this._resizeHint();
-//			}
-//		},
-//		
-//		_hideHint: function () {
-//			this.hint.hide();
-//		},
-//		
-//		_resizeHint: function () {
-//			this.hint
-//				.outerWidth(this.element.outerWidth())
-//				.position($.extend({ of: this.element }, this.options.position));
-//		}
         
-        
-        
-        
-        
-        
-        
-	});
+
 }(jQuery, window));
 
